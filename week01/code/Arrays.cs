7ca7841472624eb8ff975d5ee95ce0c81b5efb86
Nedie Yassin <yassinnedie9121@gmail.com>
2026-03-07 22:10:36 +0200

    w1

--- week01/code/Arrays.cs
+++ week01/code/Arrays.cs
@@ -13,7 +13,20 @@ public static class Arrays
         // step by step before you write the code. The plan should be clear enough that it could
         // be implemented by another person.
 
-        return []; // replace this return statement with your own
+        // create an array of doubles with the specified length
+        // Loop through each position in the array and set the value to be the number multiplied by the position (starting at 1)
+        // return the array
+
+        // create an array of doubles with the specified length
+        var result = new double[length];
+
+        // Loop through each position in the array and set the value to be the number multiplied by the position (starting at 1)
+        for (var i = 0; i < length; i++)
+        {
+            result[i] = number * (i + 1);
+        }
+
+        return result;
     }
 
     /// <summary>
@@ -29,5 +42,24 @@ public static class Arrays
         // Remember: Using comments in your program, write down your process for solving this problem
         // step by step before you write the code. The plan should be clear enough that it could
         // be implemented by another person.
+
+        // Loop 'amount' times, and in each loop do the following:
+        // Get the last element of the list
+        // Remove the last element from the list
+        // Insert the last element at the beginning of the list
+
+
+        // Loop 'amount' times, and in each loop do the following:
+        for (int i = 0; i < amount; i++)
+        {
+            // Get the last element of the list
+            int last = data[data.Count - 1];
+
+            // Remove the last element from the list
+            data.RemoveAt(data.Count - 1);
+
+            //  Insert the last element at the beginning of the list
+            data.Insert(0, last);
+        }
     }
 }
