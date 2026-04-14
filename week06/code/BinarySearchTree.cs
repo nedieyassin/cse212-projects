bca2fcceb9827a9a91b36b35a9eeee5fc81d3b62
Nedie Yassin <yassinnedie9121@gmail.com>
2026-04-15 00:47:01 +0200

    w6

--- week06/code/BinarySearchTree.cs
+++ week06/code/BinarySearchTree.cs
@@ -80,9 +80,13 @@ public class BinarySearchTree : IEnumerable<int>
 
     private void TraverseBackward(Node? node, List<int> values)
     {
-        // TODO Problem 3
+        if (node is not null)
+        {
+            TraverseBackward(node.Right, values);
+            values.Add(node.Data);
+            TraverseBackward(node.Left, values);
+        }
     }
-
     /// <summary>
     /// Get the height of the tree
     /// </summary>
@@ -99,8 +103,10 @@ public class BinarySearchTree : IEnumerable<int>
     }
 }
 
-public static class IntArrayExtensionMethods {
-    public static string AsString(this IEnumerable array) {
+public static class IntArrayExtensionMethods
+{
+    public static string AsString(this IEnumerable array)
+    {
         return "<IEnumerable>{" + string.Join(", ", array.Cast<int>()) + "}";
     }
 }
